c4abb90822cdf9fc8accb8b882985e105a5738d9
Lakshmi Priya D <07lakshmipriya2004@gmail.com>
2025-07-30 12:27:18 +0530

    Assignment 4 updated!!

---
+++ C2TC_Assignment/src/Assignment4/InterfaceDemo.java
@@ -0,0 +1,15 @@
+package Assignment4;
+
+public class InterfaceDemo {
+	 public static void main(String[] args) {
+	        AirIndia ai = new AirIndia(3, 1500.0);
+	        ai.display();
+
+	        Indigo indigo = new Indigo(2, 2000.0);
+	        indigo.display();
+
+	        KingFisher kf = new KingFisher(4, 1200.0);
+	        kf.display();
+	    }
+
+}
